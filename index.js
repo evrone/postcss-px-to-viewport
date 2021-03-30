719eaab420ca913a45ac94b14d4ba825b100e8cc
huyanhai <810839700@qq.com>
2021-03-30 16:46:59 +0800

    fix: postcss.atRule is not a constructor

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-'use strict';
+"use strict";
 
-var postcss = require('postcss');
-var objectAssign = require('object-assign');
-var { createPropListMatcher } = require('./src/prop-list-matcher');
-var { getUnitRegexp } = require('./src/pixel-unit-regexp');
+var postcss = require("postcss");
+var objectAssign = require("object-assign");
+var { createPropListMatcher } = require("./src/prop-list-matcher");
+var { getUnitRegexp } = require("./src/pixel-unit-regexp");
 
 var defaults = {
-  unitToConvert: 'px',
+  unitToConvert: "px",
   viewportWidth: 320,
   viewportHeight: 568, // not now used; TODO: need for different units and math for different properties
   unitPrecision: 5,
-  viewportUnit: 'vw',
-  fontViewportUnit: 'vw',  // vmin is more suitable.
+  viewportUnit: "vw",
+  fontViewportUnit: "vw", // vmin is more suitable.
   selectorBlackList: [],
-  propList: ['*'],
+  propList: ["*"],
   minPixelValue: 1,
   mediaQuery: false,
   replace: true,
   landscape: false,
-  landscapeUnit: 'vw',
-  landscapeWidth: 568
+  landscapeUnit: "vw",
+  landscapeWidth: 568,
 };
 
-var ignoreNextComment = 'px-to-viewport-ignore-next';
-var ignorePrevComment = 'px-to-viewport-ignore';
+var ignoreNextComment = "px-to-viewport-ignore-next";
+var ignorePrevComment = "px-to-viewport-ignore";
 
-module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
+module.exports = postcss.plugin("postcss-px-to-viewport", function (options) {
   var opts = objectAssign({}, defaults, options);
 
-  checkRegExpOrArray(opts, 'exclude');
-  checkRegExpOrArray(opts, 'include');
+  checkRegExpOrArray(opts, "exclude");
+  checkRegExpOrArray(opts, "include");
 
   var pxRegex = getUnitRegexp(opts.unitToConvert);
   var satisfyPropList = createPropListMatcher(opts.propList);
@@ -41,9 +41,9 @@ module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
       var file = rule.source && rule.source.input.file;
 
       if (opts.include && file) {
-        if (Object.prototype.toString.call(opts.include) === '[object RegExp]') {
+        if (Object.prototype.toString.call(opts.include) === "[object RegExp]") {
           if (!opts.include.test(file)) return;
-        } else if (Object.prototype.toString.call(opts.include) === '[object Array]') {
+        } else if (Object.prototype.toString.call(opts.include) === "[object Array]") {
           var flag = false;
           for (var i = 0; i < opts.include.length; i++) {
             if (opts.include[i].test(file)) {
@@ -56,9 +56,9 @@ module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
       }
 
       if (opts.exclude && file) {
-        if (Object.prototype.toString.call(opts.exclude) === '[object RegExp]') {
+        if (Object.prototype.toString.call(opts.exclude) === "[object RegExp]") {
           if (opts.exclude.test(file)) return;
-        } else if (Object.prototype.toString.call(opts.exclude) === '[object Array]') {
+        } else if (Object.prototype.toString.call(opts.exclude) === "[object Array]") {
           for (var i = 0; i < opts.exclude.length; i++) {
             if (opts.exclude[i].test(file)) return;
           }
@@ -70,13 +70,15 @@ module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
       if (opts.landscape && !rule.parent.params) {
         var landscapeRule = rule.clone().removeAll();
 
-        rule.walkDecls(function(decl) {
+        rule.walkDecls(function (decl) {
           if (decl.value.indexOf(opts.unitToConvert) === -1) return;
           if (!satisfyPropList(decl.prop)) return;
 
-          landscapeRule.append(decl.clone({
-            value: decl.value.replace(pxRegex, createPxReplace(opts, opts.landscapeUnit, opts.landscapeWidth))
-          }));
+          landscapeRule.append(
+            decl.clone({
+              value: decl.value.replace(pxRegex, createPxReplace(opts, opts.landscapeUnit, opts.landscapeWidth)),
+            })
+          );
         });
 
         if (landscapeRule.nodes.length > 0) {
@@ -86,22 +88,22 @@ module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
 
       if (!validateParams(rule.parent.params, opts.mediaQuery)) return;
 
-      rule.walkDecls(function(decl, i) {
+      rule.walkDecls(function (decl, i) {
         if (decl.value.indexOf(opts.unitToConvert) === -1) return;
         if (!satisfyPropList(decl.prop)) return;
 
         var prev = decl.prev();
         // prev declaration is ignore conversion comment at same line
-        if (prev && prev.type === 'comment' && prev.text === ignoreNextComment) {
+        if (prev && prev.type === "comment" && prev.text === ignoreNextComment) {
           // remove comment
           prev.remove();
           return;
         }
         var next = decl.next();
         // next declaration is ignore conversion comment at same line
-        if (next && next.type === 'comment' && next.text === ignorePrevComment) {
+        if (next && next.type === "comment" && next.text === ignorePrevComment) {
           if (/\n/.test(next.raws.before)) {
-            result.warn('Unexpected comment /* ' + ignorePrevComment + ' */ must be after declaration at same line.', { node: next });
+            result.warn("Unexpected comment /* " + ignorePrevComment + " */ must be after declaration at same line.", { node: next });
           } else {
             // remove comment
             next.remove();
@@ -113,7 +115,7 @@ module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
         var size;
         var params = rule.parent.params;
 
-        if (opts.landscape && params && params.indexOf('landscape') !== -1) {
+        if (opts.landscape && params && params.indexOf("landscape") !== -1) {
           unit = opts.landscapeUnit;
           size = opts.landscapeWidth;
         } else {
@@ -134,9 +136,9 @@ module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
     });
 
     if (landscapeRules.length > 0) {
-      var landscapeRoot = new postcss.atRule({ params: '(orientation: landscape)', name: 'media' });
+      var landscapeRoot = postcss.atRule({ params: "(orientation: landscape)", name: "media" });
 
-      landscapeRules.forEach(function(rule) {
+      landscapeRules.forEach(function (rule) {
         landscapeRoot.append(rule);
       });
       css.append(landscapeRoot);
@@ -145,7 +147,7 @@ module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
 });
 
 function getUnit(prop, opts) {
-  return prop.indexOf('font') === -1 ? opts.viewportUnit : opts.fontViewportUnit;
+  return prop.indexOf("font") === -1 ? opts.viewportUnit : opts.fontViewportUnit;
 }
 
 function createPxReplace(opts, viewportUnit, viewportSize) {
@@ -153,49 +155,49 @@ function createPxReplace(opts, viewportUnit, viewportSize) {
     if (!$1) return m;
     var pixels = parseFloat($1);
     if (pixels <= opts.minPixelValue) return m;
-    var parsedVal = toFixed((pixels / viewportSize * 100), opts.unitPrecision);
-    return parsedVal === 0 ? '0' : parsedVal + viewportUnit;
+    var parsedVal = toFixed((pixels / viewportSize) * 100, opts.unitPrecision);
+    return parsedVal === 0 ? "0" : parsedVal + viewportUnit;
   };
 }
 
 function error(decl, message) {
-  throw decl.error(message, { plugin: 'postcss-px-to-viewport' });
+  throw decl.error(message, { plugin: "postcss-px-to-viewport" });
 }
 
 function checkRegExpOrArray(options, optionName) {
   var option = options[optionName];
   if (!option) return;
-  if (Object.prototype.toString.call(option) === '[object RegExp]') return;
-  if (Object.prototype.toString.call(option) === '[object Array]') {
+  if (Object.prototype.toString.call(option) === "[object RegExp]") return;
+  if (Object.prototype.toString.call(option) === "[object Array]") {
     var bad = false;
     for (var i = 0; i < option.length; i++) {
-      if (Object.prototype.toString.call(option[i]) !== '[object RegExp]') {
+      if (Object.prototype.toString.call(option[i]) !== "[object RegExp]") {
         bad = true;
         break;
       }
     }
     if (!bad) return;
   }
-  throw new Error('options.' + optionName + ' should be RegExp or Array of RegExp.');
+  throw new Error("options." + optionName + " should be RegExp or Array of RegExp.");
 }
 
 function toFixed(number, precision) {
   var multiplier = Math.pow(10, precision + 1),
     wholeNumber = Math.floor(number * multiplier);
-  return Math.round(wholeNumber / 10) * 10 / multiplier;
+  return (Math.round(wholeNumber / 10) * 10) / multiplier;
 }
 
 function blacklistedSelector(blacklist, selector) {
-  if (typeof selector !== 'string') return;
+  if (typeof selector !== "string") return;
   return blacklist.some(function (regex) {
-    if (typeof regex === 'string') return selector.indexOf(regex) !== -1;
+    if (typeof regex === "string") return selector.indexOf(regex) !== -1;
     return selector.match(regex);
   });
 }
 
 function declarationExists(decls, prop, value) {
   return decls.some(function (decl) {
-      return (decl.prop === prop && decl.value === value);
+    return decl.prop === prop && decl.value === value;
   });
 }
 
